15dee1b56393c15d81982b7eca2167744603ddc7
yt2226creer <yzr9422tsuji@gmail.com>
2024-11-04 21:52:48 +0900

    11.4kadai-yoru

---
+++ JsPractice/第４章.js
@@ -0,0 +1,16 @@
+var Number=1;
+console.log(Number);
+var Text="hunger";
+console.log(hunger);
+var Boolean=true
+console.log(true);
+var addNumber=10+20;
+console.log(addNumber);
+var addNumber=10-20;
+console.log(addNumber);
+var addNumber=50*2;
+console.log(addNumber);
+var addNumber=20/2;
+console.log(addNumber);
+var combinationText="12"+"34";
+console.log(combinationTex);
